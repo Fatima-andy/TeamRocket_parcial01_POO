4eae27ef2e3e4b9b95bef9933d68dfe378eeab34
RIMP-19 <00156319@uca.edu.sv>
2020-04-19 11:11:02 -0600

    ADD: Class CalculadoraImpuestos and PlazaFija.

---
+++ Parcial-01/src/com/FAQE/x00139619/CalculadoraImpuestos.java
@@ -0,0 +1,18 @@
+package com.FAQE.x00139619;
+
+public final class CalculadoraImpuestos {
+    private double totalRenta, totalISSS, totalAFP;
+
+    private CalculadoraImpuestos() {
+    }
+
+    public double calcularPago(Empleado colaborador) {
+        double salarioNeto = colaborador.salario;
+        double pago = 0;
+        if (colaborador instanceof PlazaFija) {
+
+        }
+        return pago;
+    }
+
+}
---
+++ Parcial-01/src/com/FAQE/x00139619/PlazaFija.java
@@ -0,0 +1,7 @@
+package com.FAQE.x00139619;
+
+public class PlazaFija extends Empleado {
+    public PlazaFija(String nombre, String puesto, double salario) {
+        super(nombre, puesto, salario);
+    }
+}
